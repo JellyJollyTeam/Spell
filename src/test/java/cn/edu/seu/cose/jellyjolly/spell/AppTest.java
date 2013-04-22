b6a966be79c347960a939b3548d37f2ce2edf4ba
Ray <Ray@192.168.0.100>
2013-04-22 16:26:20 +0800

    完成spell分析器的接口设计；编写部分单元测试

---
+++ src/test/java/cn/edu/seu/cose/jellyjolly/spell/AppTest.java
@@ -0,0 +1,57 @@
+package cn.edu.seu.cose.jellyjolly.spell;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+
+    public AppTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testSpellParser() throws IOException {
+        SpellParser parser = null; // construct the concrete spell parser here
+        String source = getSourceFromClassPath();
+        Quiz quiz = parser.getQuiz(source);
+        testQuiz(quiz);
+    }
+
+    private void testQuiz(Quiz quiz) {
+        QuizTester tester = new QuizTester();
+        quiz.accept(tester);
+    }
+
+    private String getSourceFromClassPath() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        InputStream in = AppTest.class.getClassLoader().getResourceAsStream(
+                "cn/edu/seu/cose/jellyjolly/spell/sample.spell");
+        byte[] buffer = new byte[1024];
+        while (true) {
+            int len = in.read(buffer);
+            if (len <= 0) {
+                break;
+            }
+            baos.write(buffer, 0, len);
+        }
+        byte[] byteArr = baos.toByteArray();
+        String source = new String(byteArr, Charset.forName("UTF-8"));
+        System.out.println("Source:");
+        System.out.println();
+        System.out.println(source);
+        System.out.println();
+        return source;
+    }
+}
